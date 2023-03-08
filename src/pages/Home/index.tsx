7b33bd2553bd4b12fda8b9c05f045c580bfc5f98
Deivid Marlon <deividmarlon@gmail.com>
2023-03-07 21:06:44 -0300

    example changes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,7 +9,7 @@ export function Home(){
       <section className="container-with-nav bg-cover">
       <div className="w-full h-full flex flex-col items-center justify-center gap-4">
         <h1 className="text-white outline-title-3 font-medium text-7xl">SoftIndex</h1>
-        <h2 className="text-white outline-title-1 font-medium text-2xl">FRASE EXTREMAMENTE LEGAL AQUI</h2>
+        <h2 className="text-white outline-title-1 font-medium text-2xl">Gerenciamento de Portfólio de Software</h2>
         <div className="flex gap-4">
         </div>
       </div>
